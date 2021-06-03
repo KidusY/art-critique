10651394ce0a2e800c371ff1dd6e69c1ef2b74ac
Kidus <kidusyilma@gmail.com>
2021-06-03 11:26:08 -0700

    added endpoit

--- routes/posts.js
+++ routes/posts.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Posts = require("../models/posts");
-const { google } = require('googleapis');
+// const { google } = require('googleapis');
 const { v4: uuidv4 } = require('uuid');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs')
-const CLIENT_ID = '349294232179-q6i7330a6m60eu3t42kcdivnj21aldij.apps.googleusercontent.com';
-const CLIENT_SECRET = 'Rli79vH5-dKM33wf5R7p7y9d';
-const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04Q3Y1qku5FoxCgYIARAAGAQSNwF-L9Ir9TW99N6wNYb9mgiDVy7u50L1FCzedbTbD-lRgZd8-tyLZlh81ryjY7qBKtjX5xYSy-g';
+// const multer = require('multer');
+// const path = require('path');
+// const fs = require('fs')
+// const CLIENT_ID = '349294232179-q6i7330a6m60eu3t42kcdivnj21aldij.apps.googleusercontent.com';
+// const CLIENT_SECRET = 'Rli79vH5-dKM33wf5R7p7y9d';
+// const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
+// const REFRESH_TOKEN = '1//04Q3Y1qku5FoxCgYIARAAGAQSNwF-L9Ir9TW99N6wNYb9mgiDVy7u50L1FCzedbTbD-lRgZd8-tyLZlh81ryjY7qBKtjX5xYSy-g';
 
-const oauthClient = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URL
-)
+// const oauthClient = new google.auth.OAuth2(
+//     CLIENT_ID,
+//     CLIENT_SECRET,
+//     REDIRECT_URL
+// )
 
-oauthClient.setCredentials({ refresh_token: REFRESH_TOKEN });
+// oauthClient.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-const drive = google.drive({
-    version: 'v3',
-    auth: oauthClient
+// const drive = google.drive({
+//     version: 'v3',
+//     auth: oauthClient
 
-})
+// })
 
-const upload = multer({
-    dest: __dirname + '../public/uploads/',
-    limits: { fileSize: 1000000, files: 1 }
-})
+// const upload = multer({
+//     dest: __dirname + '../public/uploads/',
+//     limits: { fileSize: 1000000, files: 1 }
+// })
 
 
 
@@ -107,6 +107,50 @@ router.post('/', async (req, res) => {
         let newPost = await Post.save();
 
         res.json(newPost);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+
+
+
+})
+router.put('/likes', async (req, res) => {
+    const { userId, postId } = req.body;
+
+    const posts = await Posts.find({ postId: postId });
+    if (posts.length < 0) {
+        return res.status(404).json({
+            errorMessage: "No Post Found"
+        })
+    }
+
+    let isLiked = posts[0].likes.includes(userId);
+    try {
+        const filter = { postId };
+        const options = { upsert: false };
+        let newPostLikes = posts[0].likes;
+        let updateDoc = {};
+
+        if (!isLiked) {
+            newPostLikes.push(userId);
+            updateDoc = {
+                $set: {
+                    likes: newPostLikes
+                }
+            }
+        }
+        else {
+            newPostLikes = posts[0].likes.filter(likedBy => likedBy !== userId);
+            updateDoc = {
+                $set: {
+                    likes: newPostLikes
+                }
+            }
+        }
+        const updatePost = await Posts.updateOne(filter, updateDoc, options)
+        console.log(updatePost);
+        res.json(updatePost);
     }
     catch (err) {
         res.status(500).json(err);
